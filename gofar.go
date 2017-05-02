131cebf1a746bd7417c734ec6ad0494e550afd3b
DeockJin Chung <jin.freestyle@gmail.com>
2017-05-02 14:51:12 +0900

    initial

---
+++ gofar.go
@@ -0,0 +1,162 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// @project gofar
+// @author 1100282
+// @date 2017. 3. 26. PM 5:25
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"throosea.com/gofar/lib"
+	"io/ioutil"
+	"strings"
+	"time"
+	"encoding/json"
+)
+
+var resourceList []string
+var binprefix string
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "you have to specify program name\n")
+		return
+	}
+	proc := os.Args[1]
+	if len(os.Args) >= 3 {
+		binprefix = os.Args[2]
+	}
+	gopath := os.Getenv("GOPATH")
+
+	binpath, e := lib.EnsureBinary(binprefix, proc, gopath)
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+		return
+	}
+
+	fmt.Printf("binpath : %s\n", binpath)
+	resourceList = make([]string, 0)
+	findPropertyFromSrc(proc + ".", filepath.Join(gopath, "src"))
+	if len(resourceList) == 0 {
+		fmt.Fprintf(os.Stderr, "not found %s resource\n", proc)
+		return
+	}
+
+	farDir := filepath.Join(gopath, "far", proc)
+
+	lib.EnsureDirectory(farDir)
+	farName := time.Now().Format("2006-01-02_15-04-05.far")
+	farName = fmt.Sprintf("%s_%s", proc, farName)
+	farPath := filepath.Join(farDir, farName)
+
+	fmt.Printf("target : %s\n", farPath)
+
+	tmpDir, err := ioutil.TempDir("", "gofar")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+		return
+	}
+	defer os.RemoveAll(tmpDir) // clean up
+
+	// binary 복사
+	targetBin := filepath.Join(tmpDir, proc)
+	err = lib.CopyFile(binpath, targetBin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+		return
+	}
+	os.Chmod(targetBin, 0755)
+
+	for _, r := range resourceList {
+		target := filepath.Join(tmpDir, filepath.Base(r))
+		err = lib.CopyFile(r, target)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+			return
+		}
+	}
+
+	// create deployment.json
+	m := make(map[string]string)
+	m["process"] = proc
+	b, err := json.Marshal(m)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+		return
+	}
+	depfile := filepath.Join(tmpDir, "deployment.json")
+	err = ioutil.WriteFile(depfile, b, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+		return
+	}
+
+	//fmt.Printf("check tmp dir %s\n", tmpDir)
+
+	lib.Zipit(tmpDir, farPath)
+	fmt.Printf("Finished far zip : %s\n", farPath)
+}
+
+var includeSuffixList = [...]string{"properties", "xml", "json", "yaml"}
+
+func findPropertyFromSrc(proc string, path string) {
+	candidate := false
+	if strings.HasPrefix(proc, filepath.Base(path)) {
+		candidate = true
+	}
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fail to read dir : %s\n", err.Error())
+		return
+	}
+
+	dirList := make([]os.FileInfo, 0)
+
+	for _, file := range files {
+		if file.Name()[0] == '.' {
+			continue
+		}
+
+		if file.IsDir() {
+			dirList = append(dirList, file)
+		} else if candidate {
+			for _, s := range includeSuffixList {
+				if strings.HasSuffix(file.Name(), s) {
+					resourceList = append(resourceList, filepath.Join(path, file.Name()))
+					break
+				}
+			}
+		}
+	}
+
+	if len(resourceList) > 0 {
+		return
+	}
+
+	for _, dir := range dirList {
+		findPropertyFromSrc(proc, filepath.Join(path, dir.Name()))
+		if len(resourceList) > 0 {
+			return
+		}
+	}
+}
---
+++ lib/util.go
@@ -0,0 +1,158 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// @project gofar
+// @author 1100282
+// @date 2017. 3. 26. PM 5:45
+//
+
+package lib
+
+import (
+	"path/filepath"
+	"fmt"
+	"os"
+	"io"
+	"strings"
+	"archive/zip"
+)
+
+func EnsureBinary(binprefix string, proc string, gopathDir string) (string, error) {
+	var binpath string
+	if len(binprefix) == 0 {
+		binpath = filepath.Join(gopathDir, "bin", proc)
+	} else {
+		binpath = filepath.Join(gopathDir, "bin", binprefix, proc)
+	}
+
+	return binpath, CheckFileExist(binpath)
+}
+
+func EnsureDirectory(dir string) error {
+	if _, err := os.Stat(dir); err != nil {
+		if os.IsNotExist(err) {
+			return os.MkdirAll(dir, 0755)
+		}
+	}
+
+	return nil
+}
+
+func CheckFileExist(path string) error {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("not exist file : %s", path)
+		}
+	}
+
+	return nil
+}
+
+func CopyFile(src string, dst string) error {
+	fmt.Printf("src : %s\n", src)
+	sFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sFile.Close()
+
+	eFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer eFile.Close()
+
+	_, err = io.Copy(eFile, sFile) // first var shows number of bytes
+	if err != nil {
+		return err
+	}
+
+	err = eFile.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+func Zipit(source, target string) error {
+	zipfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipfile.Close()
+
+	archive := zip.NewWriter(zipfile)
+	defer archive.Close()
+
+	_, err = os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	//if info.IsDir() {
+	//	baseDir = filepath.Base(source)
+	//}
+
+	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		//fmt.Printf("zip path : %s\n", path)
+		if source == path {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
+}
